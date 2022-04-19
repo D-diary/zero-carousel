70a2eed38da530a699c21659d673672354ececf7
D-diary <dlrlsud@naver.com>
2022-04-20 02:10:26 +0900

    movePrev, moveNext

--- practice/script.js
+++ practice/script.js
@@ -18,8 +18,35 @@
     setEventListener() {
       this.prevButton = this.carouselElement.querySelector('.carousel_button--prev')
       this.nextButton = this.carouselElement.querySelector('.carousel_button--next')
+      this.prevButton.addEventListener('click', () => { this.movePrev() })
+      this.nextButton.addEventListener('click', () => { this.moveNext() })
+    }
+
+    moveCarousleTo() {
+      let prev = this.current - 1
+      let next = this.current + 1
+    }
+
+    moveNext() {
+      if (this.current === this.totalItems - 1) {
+        this.current = 0
+      } else { 
+        this.current++
+      }
+      this.moveCarouselTo()
+    }
+
+    movePrev() {
+      if (this.current === 0) {
+        this.current = this.totalItems - 1
+      } else {
+        this.current--
+      }
+      this.moveCarouselTo()
+
     }
   }
+
 
 
   document.addEventListener('DOMContentLoaded',() => {
